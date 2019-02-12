0049e87fa470479ed94f926e9a4544fbdc1c9cb8
IoanCristianRadu <raduioancristian@gmail.com>
2019-02-12 20:45:53 +0100

    P1 problem requirements

--- src/P1/P1.java
+++ src/P1/P1.java
@@ -1,9 +1,15 @@
-package leetcode.P1;
+package P1;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class T1 {
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//      Given nums = [2, 7, 11, 15], target = 9,
+//      Because nums[0] + nums[1] = 2 + 7 = 9,
+//      return [0, 1].
+
+public class P1 {
     public static void main(String[] args){
         Solution solution = new Solution();
 
