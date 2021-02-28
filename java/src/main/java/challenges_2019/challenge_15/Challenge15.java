a612a9d77383465eae6c1293602b1df040cfba17
Cristian Radu <raduioancristian@gmail.com>
2021-02-28 16:30:07 +0100

    DEV-0005: Split the project into java and python

---
+++ java/src/main/java/challenges_2019/challenge_15/Challenge15.java
@@ -0,0 +1,42 @@
+package challenges_2019.challenge_15;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+// Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+// Find all unique triplets in the array which gives the sum of zero.
+// The solution set must not contain duplicate triplets.
+
+class Challenge15 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.bruteForce(new int[]{-1, 0, 1, 2, -1, -4}));
+    }
+}
+
+class Solution {
+    public List<List<Integer>> bruteForce(int[] numbers) {
+        List<List<Integer>> solution = new ArrayList<>();
+
+        if (numbers.length < 3) {
+            return new ArrayList<>();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                for (int k = j + 1; k < numbers.length; k++) {
+                    if (numbers[i] + numbers[j] + numbers[k] == 0) {
+                        List<Integer> solutionSet = new ArrayList<>();
+
+                        solutionSet.add(numbers[i]);
+                        solutionSet.add(numbers[j]);
+                        solutionSet.add(numbers[k]);
+                        solutionSet.sort(Comparator.comparingInt(a -> a));
+                        solution.add(solutionSet);
+                    }
+                }
+            }
+        }
+        return solution.stream().distinct().collect(Collectors.toList());
+    }
+}
---
+++ java/src/main/java/challenges_2019/challenge_9/Challenge9.java
@@ -0,0 +1,28 @@
+package challenges_2019.challenge_9;
+
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+//      Input: 121
+//      Output: true
+//
+// Runtime: 73 ms, faster than 98.74% of Java online submissions for Palindrome Number.
+// Memory Usage: 38.9 MB, less than 99.60% of Java online submissions for Palindrome Number.
+
+public class Challenge9 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(1234321));
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(int testedNumber) {
+        int reverse = 0, actual = testedNumber;
+        if (testedNumber < 0) return false;
+
+        while (actual != 0) {
+            reverse = reverse * 10 + actual % 10;
+            actual = actual / 10;
+        }
+        return reverse == testedNumber;
+    }
+}
